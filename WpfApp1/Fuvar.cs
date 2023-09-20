7d6d62466855deb292f3cc85ce4817165f15b40b
bankuti.laszlo <bankuti.laszlo@58-16>
2023-09-20 11:40:43 +0200

    Add project files.

---
+++ WpfApp1/Fuvar.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class Fuvar
+    {
+        int taxiAzonosito;
+        string indulas;
+        int idotartam;
+        float tav;
+        float viteldij;
+        float borravalo;
+        string fizetes;
+
+        public Fuvar(int taxiAzonosito, string indulas, int idotartam, float tav, float viteldij, float borravalo, string fizetes)
+        {
+            this.taxiAzonosito = taxiAzonosito;
+            this.indulas = indulas;
+            this.idotartam = idotartam;
+            this.tav = tav;
+            this.viteldij = viteldij;
+            this.borravalo = borravalo;
+            this.fizetes = fizetes;
+        }
+
+        public int TaxiAzonosito { get => taxiAzonosito;  }
+        public string Indulas { get => indulas; }
+        public int Idotartam { get => idotartam; }
+        public float Tav { get => tav; }
+        public float Viteldij { get => viteldij; }
+        public float Borravalo { get => borravalo; }
+        public string Fizetes { get => fizetes; }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Fuvar> fuvarok = new List<Fuvar>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            StreamReader sr = new StreamReader("fuvar.csv");
+            sr.ReadLine();
+            while (!sr.EndOfStream){
+                string[] adat = sr.ReadLine().Split(";");
+                fuvarok.Add(new Fuvar(int.Parse(adat[0]), adat[1], int.Parse(adat[2]), float.Parse(adat[3]), float.Parse(adat[4]), float.Parse(adat[5]), adat[6]));
+            }
+            sr.Close();
+        }
+
+        private void btnHarmadik_Click(object sender, RoutedEventArgs e)
+        {
+            harmadikFeladat.Content = $"3. feladat: {fuvarok.Count()} fuvar";
+        }
+
+        private void btnNegyedik_Click(object sender, RoutedEventArgs e)
+        {
+            negyedikFeladat.Content = $"4. feladat: {fuvarok.Count(x => x.TaxiAzonosito == 6185)} fuvar alatt: {fuvarok.Where(x=>x.TaxiAzonosito == 6185).Sum(x=>x.Viteldij)}$";
+        }
+
+        private void btnOtodik_Click(object sender, RoutedEventArgs e)
+        {
+            otodikFeladat.Items.Add("5. feladat:");
+            fuvarok.GroupBy(x => x.Fizetes).ToList().ForEach(x => otodikFeladat.Items.Add($"\t{x.Key}: {x.Count()} fuvar"));
+        }
+
+        private void btnHatodik_Click(object sender, RoutedEventArgs e)
+        {
+            hatodikFeladat.Content = $"{Math.Round(fuvarok.Sum(x => x.Tav * 1.6), 2)}km";
+        }
+
+        private void btnNyolcadik_Click(object sender, RoutedEventArgs e)
+        {
+            nyolcadikFeladat.Content = "8. feladat: hibak.txt";
+            using (StreamWriter writer = new StreamWriter("hibak.txt"))
+            {
+                writer.WriteLine("fuvar_id;indulas;idotartam;tavolsag;viteldij;borravalo;fizetes_modja");
+                fuvarok.Where(x => x.Idotartam > 0 && x.Viteldij > 0 && x.Tav == 0).OrderBy(x => x.Indulas).ToList().ForEach
+                    (x => writer.WriteLine($"{x.TaxiAzonosito};{x.Indulas};{x.Idotartam};{x.Tav};{x.Viteldij};{x.Borravalo};{x.Fizetes}"));
+            }
+        }
+    }
+}
